590f08a1305810f01b15c748b2ba35a94b4b2e55
Lais Frigério <lais.frigerio@nubank.com.br>
2022-06-27 09:52:42 -0300

    fix: indenting with 2 spaces

--- balanced-parentheses/code/bad-solution.js
+++ balanced-parentheses/code/bad-solution.js
@@ -1,37 +1,36 @@
 function isExpressionBalanced (expression) {
-    if (expression.length === 0 || expression.length % 2 === 1) {
-        return false
-    }
+  if (expression.length === 0 || expression.length % 2 === 1) {
+    return false
+  }
 
-    const size = expression.length
-    const stack = []
+  const size = expression.length
+  const stack = []
 
-    for (let index = 0; index < size; index++) {
-        const element = expression[index]
+  for (let index = 0; index < size; index++) {
+    const element = expression[index]
 
-        if (element === '(' || element === '[' || element === '{') {
-            stack.push(element)
-        } else {
-            const top = stack.pop()
-            
-            if (element === ')' && top !== '(') {
-                return false
-            } else if (element === ']' && top !== '[') {
-                return false
-            } else if (element === '}' && top !== '{') {
-                return false
-            }
-        }
-    }
-
-    if (stack.length === 0) {
-        return true
+    if (element === '(' || element === '[' || element === '{') {
+      stack.push(element)
     } else {
+      const top = stack.pop()
+      
+      if (element === ')' && top !== '(') {
         return false
+      } else if (element === ']' && top !== '[') {
+        return false
+      } else if (element === '}' && top !== '{') {
+        return false
+      }
     }
+  }
+
+  if (stack.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = {
-    isExpressionBalanced
-  }
-  
+  isExpressionBalanced
+}
